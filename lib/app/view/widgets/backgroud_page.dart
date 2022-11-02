4f4c9be5b5269f57c069f7613ef48cac0b8894f0
Gabriel Andrella <gabriel@inovaclick.com.br>
2022-11-02 16:25:24 -0300

    Add banner in view app

--- lib/app/view/widgets/backgroud_page.dart
+++ lib/app/view/widgets/backgroud_page.dart
@@ -1,20 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'dart:io';
 
-class BackgroundPage extends StatelessWidget {
+class BackgroundPage extends StatefulWidget {
   final BoxConstraints constraints;
 
   const BackgroundPage({Key? key, required this.constraints}) : super(key: key);
+
+  @override
+  State<BackgroundPage> createState() => _BackgroundPageState();
+}
+
+class _BackgroundPageState extends State<BackgroundPage> {
+  InterstitialAd? _interstitialAd;
+
+  BannerAd? _bannerAd;
+
+  @override
+  void initState() {
+    super.initState();
+
+    loadInterstitialAd();
+    loadBannerAd();
+  }
+
+  void loadBannerAd() {
+    _bannerAd = BannerAd(
+      adUnitId: Platform.isIOS == TargetPlatform.iOS
+          ? "ca-app-pub-3940256099942544/2934735716"
+          : "ca-app-pub-3940256099942544/6300978111",
+      size: AdSize.banner,
+      request: AdRequest(),
+      listener: BannerAdListener(),
+    );
+
+    _bannerAd?.load();
+  }
+
+  void loadInterstitialAd() {
+    String interstitialAdId = Platform.isIOS == TargetPlatform.iOS
+        ? "ca-app-pub-3940256099942544/4411468910"
+        : "ca-app-pub-3940256099942544/1033173712";
+    InterstitialAd.load(
+        adUnitId: interstitialAdId,
+        request: AdRequest(),
+        adLoadCallback: InterstitialAdLoadCallback(
+          onAdLoaded: (InterstitialAd ad) {
+
+            this._interstitialAd = ad;
+
+            ad.fullScreenContentCallback = FullScreenContentCallback(
+              onAdDismissedFullScreenContent: (InterstitialAd ad) {
+                ad.dispose();
+                loadInterstitialAd();
+              },
+              onAdFailedToShowFullScreenContent:
+                  (InterstitialAd ad, AdError error) {
+                ad.dispose();
+                loadInterstitialAd();
+              },
+            );
+          },
+          onAdFailedToLoad: (LoadAdError error) {
+            print('InterstitialAd failed to load: $error');
+          },
+        ));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Container(
-          height: constraints.maxHeight * 0.4,
-          width: constraints.maxWidth,
-          color: Color.fromRGBO(255, 222, 89,1),
+          height: widget.constraints.maxHeight * 0.4,
+          width: widget.constraints.maxWidth,
+          color: Color.fromRGBO(255, 222, 89, 1),
           child: Padding(
-            padding: EdgeInsets.only(bottom: constraints.maxHeight * 0.1),
+            padding:
+                EdgeInsets.only(bottom: widget.constraints.maxHeight * 0.1),
             child: Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -23,9 +86,16 @@ class BackgroundPage extends StatelessWidget {
                     "Daily Jokes",
                     style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
                   ),
-                  Text(
+                  TextButton(
+                    onPressed: () { 
+                      _interstitialAd?.show();
+                     },
+                    child: Text(
                     "Your daily dose of humor",
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500), 
+                    
+                    ) 
+                    
                   ),
                 ],
               ),
@@ -33,9 +103,15 @@ class BackgroundPage extends StatelessWidget {
           ),
         ),
         Container(
-          height: constraints.maxHeight * 0.6,
-          width: constraints.maxWidth,
+          height: widget.constraints.maxHeight * 0.5,
+          width: widget.constraints.maxWidth,
           color: Colors.grey[100],
+        ),
+        Container(
+          alignment: Alignment.center,
+          child: AdWidget(ad: _bannerAd!),
+          height: widget.constraints.maxHeight * 0.1,
+          width: widget.constraints.maxWidth,
         ),
       ],
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:daily_jokes/app/controller/home_controller.dart';
 import 'package:daily_jokes/app/view/splash_page.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 
 void main() {
   GetIt getIt = GetIt.I;
   getIt.registerSingleton<HomeController>(HomeController());
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
   runApp(MyApp());
 }
 
